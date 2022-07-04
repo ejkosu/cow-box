06944a427169413b4eb155fcf64fcf551bd94ae7
anonymous <anonymous>
2022-07-04 15:18:42 -0700

    Add initial styling and functionality to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,54 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
+    const [showBurger, setShowBurger] = useState(false);
+
     return (
-      <div id="navbar">
-        <p>Hello navbar</p>
+      <div id="navbar" className="navbar is-white-bis">
+        <div className="container">
+          <div className="navbar-brand">
+            {/* LOGO */}
+            <div className="navbar-item">
+              <img id="nav-logo" src="/src/images/logo.png" />
+            </div>
+
+            {/* TITLE */}
+            <div
+              id="nav-title"
+              className="navbar-item is-size-2 is-size-4-mobile is-unselectable"
+            >
+              Pizza-Box
+            </div>
+
+            {/* BURGER MENU */}
+            <a
+              role="button"
+              className={`navbar-burger ${showBurger ? 'is-active' : ''}`}
+              onClick={() => setShowBurger(!showBurger)}
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          {/* LINKS */}
+          <div className={`navbar-menu ${showBurger ? 'is-active' : ''}`}>
+            <div className="navbar-end is-size-5">
+              <div className="navbar-item is-size-5">
+                <NavLink to="/">
+                  Home
+                </NavLink>
+              </div>
+              <div className="navbar-item is-size-5">
+                <NavLink to="/about">
+                  About
+                </NavLink>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
