b81ad3b47e9e3777e2c3b1b46ab4b4fe7d500a3d
anonymous <anonymous>
2022-07-04 13:27:58 -0700

    Setup CSS and react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
