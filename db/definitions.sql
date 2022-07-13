9c5c39419afc74c9c6ea5f2d4744b9d26e48eec6
anonymous <anonymous>
2022-07-12 23:29:43 -0700

    Fix typo in DB uploads table definition

--- db/definitions.sql
+++ db/definitions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS uploads (
     id      SERIAL PRIMARY KEY,
-    name    TEXT UNIQUE NOT NULL CHECK (length(text) < 71),
+    name    TEXT UNIQUE NOT NULL CHECK (length(name) < 71),
     size    INTEGER NOT NULL,
     time    TIMESTAMPTZ NOT NULL
 );
